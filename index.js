3c1ad716a1098b35208f7cceed1cfecabf10d53a
Jose Barrios <jose@cognilab.com>
2017-07-02 15:34:00 -0700

    Added verifiedEmail property and tests

--- index.js
+++ index.js
@@ -3,23 +3,40 @@ const bcrypt = require('bcryptjs');
 
 class User extends Person {
 
-  constructor(model){
-    console.log(model)
-    model = model || {};
-    super(model);
-    this.password = model.password;
-  }
-
-  get password(){ return this.computed.password; }
-  set password(value){
+  static hashPassword(value){
     if(User.isString(value) || User.isNumber(value)){
       var salt = bcrypt.genSaltSync(10);
       var hash = bcrypt.hashSync(value, salt);
-      this.computed.password = hash;
+      return hash;
     } else {
       console.error('Password must be string or numbers')
     }
   }
+
+  constructor(model){
+    console.log(model)
+    model = model || {};
+    super(model);
+
+    this.password = model.password;
+    this.verifiedEmail = model.verifiedEmail;
+  }
+
+  get password(){ return this.computed.password; }
+  set password(value){ this.computed.password = User.hashPassword(value); }
+
+  get verifiedEmail(){ return this.computed.verifiedEmail; }
+  set verifiedEmail(value){
+    if(User.isBoolean(value)){
+      this.computed.verifiedEmail = value;
+    } else if(!this.computed.verifiedEmail){
+      //Default to false
+      this.computed.verifiedEmail = false;
+    } else if(User.isEmpty(this.computed.verifiedEmail)){
+      console.error("user's verifiedEmail should be boolean");
+    }
+  }
+
 
   //Retuns a user without private
   //variables, like password
@@ -29,6 +46,9 @@ class User extends Person {
     return user;
   }
 
+  passwordEquals(value){
+    return bcrypt.compareSync(value, this.password)
+  }
 }
 
 
--- tests/test.js
+++ tests/test.js
@@ -9,11 +9,9 @@ model.email = 'jose@barrios.io';
 model.password = 'abc123'
 let user = new User(model);
 
-
 var key = {};
 key.fullName = 'Jose Barrios';
 key.email = null;
-
 
 describe('user.password', function() {
 	it('should hash user password', function() {
@@ -27,8 +25,31 @@ describe('user.fullName', function() {
 	});
 });
 
+describe('user.verifiedEmail', function() {
+	it('should return the given and family names in a single string', function() {
+		assert.equal(user.verifiedEmail, false);
+    user.verifiedEmail = true;
+		assert.equal(user.verifiedEmail, true);
+    user.verifiedEmail = false;
+		assert.equal(user.verifiedEmail, false);
+    user.verifiedEmail = '';
+		assert.equal(user.verifiedEmail, false);
+    user.verifiedEmail = null;
+		assert.equal(user.verifiedEmail, false);
+    user.verifiedEmail = 1;
+		assert.equal(user.verifiedEmail, false);
+	});
+});
+
 describe('#clean', function() {
 	it('should return the user object, minus password', function() {
 		assert.equal(user.clean().password, undefined);
+	});
+});
+
+describe('#passwordEquals', function() {
+	it('should return true if unhashed password matches hashed password', function() {
+		assert.equal(user.passwordEquals(model.password), true);
+    assert.equal(user.passwordEquals('should not match'), false);
 	});
 });
