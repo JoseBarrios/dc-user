19940f6b226792fd812fc275152cacc84a9b879f
Jose Barrios <jose@cognilab.com>
2017-07-02 12:54:52 -0700

    Removed all the unecesary dependencies, removed db utils, added automatic password encryption and clean() method

--- index.js
+++ index.js
@@ -1,97 +1,34 @@
 const Person = require('@josebarrios/person');
-const bcrypt = require('bcrypt');
-const ObjectID = require('mongodb').ObjectId;
-var mongoose = require("mongoose");
-var Schema = mongoose.Schema;
-
-
-const COLLECTION_NAME = 'users';
-const SALT_ROUNDS = 10;
+const bcrypt = require('bcryptjs');
 
 class User extends Person {
 
-  static insert(user, db){
-    return new Promise(function(resolve, reject){
-      bcrypt.hash(user.password, SALT_ROUNDS).then(function(hash) {
-        user.password = hash;
-        db.insertDocument(COLLECTION_NAME, user).then(result =>{
-          resolve(result.insertedIds[0]);
-        }).catch(reject);
-      })
-    })
-  }
-
-  static getByID(identification, db){
-    return new Promise(function(resolve, reject){
-      db.getDocumentByID(COLLECTION_NAME, identification).then(user =>{
-        resolve(user);
-      }).catch(reject);
-    })
-  }
-
-  /**
-   * returns the user document based on its email property.
-   * @param  {String} email of the potential user
-   * @return {Object} user or error
-   */
-  static getByEmail(email, db){
-    let query = {};
-    query.email = email;
-    let projection = {};
-    //projection.password = 0;
-    return new Promise((resolve, reject) => {
-      db.findOne(COLLECTION_NAME, query, projection).then(user =>{
-        resolve(user)
-      }).catch(reject)
-    });
-  }
-
-
-  constructor(model, db){
+  constructor(model){
+    console.log(model)
     model = model || {};
     super(model);
-
-    this.mongoID = model._id;
-    this.collectionName = COLLECTION_NAME;
     this.password = model.password;
-    if(db){ this.enableDB(db); }
   }
 
-  get mongoID(){ return this.model.mongoID; }
-  set mongoID(value){
-    if(!value) return;
-    this.model.mongoID = value;
+  get password(){ return this.computed.password; }
+  set password(value){
+    if(User.isString(value) || User.isNumber(value)){
+      var salt = bcrypt.genSaltSync(10);
+      var hash = bcrypt.hashSync(value, salt);
+      this.computed.password = hash;
+    } else {
+      console.error('Password must be string or numbers')
+    }
   }
 
-  enableDB(db){
-    this.enabledDB = true;
-    this.db = db;
+  //Retuns a user without private
+  //variables, like password
+  clean(){
+    let user = User.assignedProperties(this)
+    delete user.password;
+    return user;
   }
 
-  disableDB(db){
-    this.enabledDB = false;
-    this.db = null;
-  }
-
-  save(){
-    return new Promise((resolve, reject) => {
-      if(this.enabledDB){
-        let updates = this.serialize();
-        this.db.updateDocument(this.collectionName, this.mongoID, updates).then(res => {
-            if(res.ok){ resolve(res.value) }
-            else { reject(res); }
-        }).catch(reject)
-      }else{ reject(new Error('You must first enable DB by passing it to #enableDB')); }
-    })
-  }
-
-  serialize(){
-    let string = JSON.stringify(this.model);
-    let serial = JSON.parse(string)
-    delete serial._id;
-    delete serial.mongoID;
-    return serial;
-  }
 }
 
 
--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,4 @@
 'use strict'
-
-var config = require('../config/config.json')
-var Mongo = require('@josebarrios/mongo-api');
-var mongo = new Mongo(config.url)
-
 var assert = require('assert');
 var User = require('../index.js');
 
@@ -11,47 +6,29 @@ var model = {};
 model.givenName = 'jose';
 model.familyName = 'barrios';
 model.email = 'jose@barrios.io';
-model.affiliation = {};
-model.affiliation.name = 'BeVisible';
-model.vatID = 1234;
-model.password = ''
+model.password = 'abc123'
 let user = new User(model);
 
 
 var key = {};
 key.fullName = 'Jose Barrios';
-key.ID = null;
 key.email = null;
 
 
-describe('User.insert', function() {
-	it('should add user to DB', function() {
-    return User.insert(model, mongo).then(userID => {
-      assert.ok(userID);
-      key.ID = userID;
-    });
-	});
-});
-
-describe('User.getByID', function() {
-	it('should return user', function() {
-    return User.getByID(key.ID, mongo).then(user => {
-      key.email = user.email;
-      assert.equal(user.email, key.email);
-    });
-	});
-});
-
-describe('User.getByEmail', function() {
-	it('should return user by email', function() {
-    return User.getByEmail(key.email, mongo).then(user => {
-      assert.equal(user.email, key.email);
-    });
+describe('user.password', function() {
+	it('should hash user password', function() {
+    assert.notEqual(user.password, model.password);
 	});
 });
 
 describe('user.fullName', function() {
 	it('should return the given and family names in a single string', function() {
 		assert.equal(user.fullName, key.fullName);
+	});
+});
+
+describe('#clean', function() {
+	it('should return the user object, minus password', function() {
+		assert.equal(user.clean().password, undefined);
 	});
 });
